21e6ce1f51761e71eaca2323435755ad1708c0fe
jack.cheng <liujuncheng@wangxiaobao.com>
2020-09-08 09:46:06 +0800

    0.0

--- src/main/java/com/liu/aop/util/LogUtil.java
+++ src/main/java/com/liu/aop/util/LogUtil.java
@@ -16,43 +16,44 @@ import java.util.Random;
  **/
 public class LogUtil {
 
-    /***
-     * 获取切点注解信息
-     * @param point AOP切面信息
-     * @return 注解信息对应实体类
-     */
     public static SysOperaLogInfo getSysOperateLogInfo(JoinPoint point) throws Exception {
-        //获取连接点目标类名，切点所属类（这里为注解方法所对应的controller类）
         String targetName = point.getTarget().getClass().getName();
-        //获取连接点签名的方法名（这里为加注解的方法）
         String methodName = point.getSignature().getName();
-        //获取连接点参数（切点方法参数）
         Object[] args = point.getArgs();
-        //根据连接点类的名字获取指定类（实例化对象)
         Class targetClass = Class.forName(targetName);
-        //获取类里面的方法（注解对应类中的所有方法）
         Method[] methods = targetClass.getMethods();
         for (Method method : methods) {
-            if (method.getName().equals(methodName)) {
-                //可能出现同方法名方法，当方法名及其参数个数都相等时，视为同一方法
-                Class[] parameterTypeClazzArr = method.getParameterTypes();
-                if (parameterTypeClazzArr.length == args.length) {
-                    for (int i = 0; i < args.length; i++) {
-                        if(args[i] instanceof HttpSession){
-                            args[i] = "session";
-                            break;
-                        }
+            if (method.getName().equals(methodName) && isParameterTypesEquals(method.getParameterTypes(), args)) {
+                for (int i = 0; i < args.length; i++) {
+                    if(args[i] instanceof HttpSession){
+                        args[i] = "session";
+                        break;
                     }
-                    return new SysOperaLogInfo(
-                            method.getAnnotation(SysOperaLog.class).desc(),
-                            method.getAnnotation(SysOperaLog.class).level(),
-                            JSON.toJSONString(args),
-                            methodName
-                    );
                 }
+                return new SysOperaLogInfo(
+                        method.getAnnotation(SysOperaLog.class).desc(),
+                        method.getAnnotation(SysOperaLog.class).level(),
+                        JSON.toJSONString(args),
+                        methodName
+                );
             }
         }
         return null;
+    }
+
+    private static boolean isParameterTypesEquals(Class<?>[] parameterTypeClazz, Object[] parameters) {
+        if (parameterTypeClazz == null) {
+            return false;
+        }
+        if (parameterTypeClazz.length != parameters.length) {
+            return false;
+        }
+        for (int i = 0; i < parameterTypeClazz.length; i++) {
+            if (!parameterTypeClazz[i].getName().equals(parameters[i].getClass().getName())) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -81,15 +82,4 @@ public class LogUtil {
         }
         return buffer.toString();
     }
-
-//    /**
-//     * 获取堆栈信息
-//     */
-//    public static String getStackTrace(Throwable throwable) {
-//        StringWriter sw = new StringWriter();
-//        try (PrintWriter pw = new PrintWriter(sw)) {
-//            throwable.printStackTrace(pw);
-//            return sw.toString();
-//        }
-//    }
 }
